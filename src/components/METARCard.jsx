36fc2a83b906769fefe106d224e8a638a7981d3d
joakha <72503684+joakha@users.noreply.github.com>
2024-09-23 17:31:11 +0300

    preliminary metarcard content added

--- src/components/METARCard.jsx
+++ src/components/METARCard.jsx
@@ -1,11 +1,52 @@
+import { useState, useEffect } from "react";
+
 const METARCard = () => {
+
+    const [metars, setMetars] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchMetarData = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch("https://api.met.no/weatherapi/tafmetar/1.0/metar?icao=EFHK%2CEFTU%2CEFTP");
+                if (!response.ok) throw new Error("Issue fetching metar data!");
+                const metarData = await response.text();
+                const metarArray = [];
+                const EFHKDataArray = metarData.split("\n").filter(line => line.startsWith("EFHK"));
+                const lastEFHKMetar = EFHKDataArray[EFHKDataArray.length - 1];
+                metarArray.push(lastEFHKMetar);
+                const EFTUDataArray = metarData.split("\n").filter(line => line.startsWith("EFTU"));
+                const lastEFTUMetar = EFTUDataArray[EFTUDataArray.length - 1];
+                metarArray.push(lastEFTUMetar)
+                const EFTPDataArray = metarData.split("\n").filter(line => line.startsWith("EFTP"));
+                const lastEFTPMetar = EFTPDataArray[EFTPDataArray.length - 1];
+                metarArray.push(lastEFTPMetar)
+                setMetars(metarArray);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchMetarData();
+    }, [])
+
     return (
         <article className="metarcard">
             <div className="metarcard-header">
                 <h3>METARS</h3>
             </div>
             <div className="metarcard-content">
-                <p>Data from METAR api goes here.</p>
+                {
+                    loading ? (
+                        <p>Loading METAR data...</p>
+                    ) : (
+                        <>
+                            {metars.map((metar, index) => <p key={index}>{metar}</p>)}
+                        </>
+                    )
+                }
             </div>
         </article>
     )
