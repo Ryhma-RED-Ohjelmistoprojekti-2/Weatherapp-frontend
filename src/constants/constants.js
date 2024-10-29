1155ab17889d480cb36307babd2b3a1922f65465
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2024-10-29 14:30:59 +0200

    Added the URL for softala -service. Still as comment due java jar problem in the service.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,6 @@
 //url for locally running backend
 //todo: switch this to use the correct url when backend is deployed
 export const dbUrl = "http://localhost:8080"
+
+//Important URL! It will link to actual deployed backend java jar in softala -service.
+//export const dbUrl = "http://http://softala.haaga-helia.fi:8082/api/weathers"
