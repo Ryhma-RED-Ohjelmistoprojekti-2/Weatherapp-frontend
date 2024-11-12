71b114722f0b56e275f1948317575965b5223d40
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2024-11-12 13:42:05 +0200

    Changed softala.fi URL as https in constant.js .

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@
 
 //This is test URL from softala -service's tmp -directory.
 //Note this is an old java -jar and is not actively runned (has to be manually runned).
-export const dbUrl = "http://softala.haaga-helia.fi:8084";
+export const dbUrl = "https://softala.haaga-helia.fi:8084";
 
 //Note the API url is
 //export const dbUrl = "http://softala.haaga-helia.fi:8084/api/weathers" 
